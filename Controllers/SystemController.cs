b603a8c81ab2ebbe22bed8900149fb70a788f969
Ashutosh Nigam <ashutosh.nigam@live.com>
2026-03-28 18:39:58 +0530

    added option to increse specific CPU percentage

--- Controllers/SystemController.cs
+++ Controllers/SystemController.cs
@@ -4,6 +4,24 @@ using System.Runtime.InteropServices;
 
 namespace VCC.Assignment3.Controllers
 {
+    public enum MemorySpikeSize
+    {
+        Small_500MB = 500,
+        Medium_1GB = 1024,
+        Large_2GB = 2048,
+        XLarge_3GB = 3072,
+        XXLarge_4GB = 4096
+    }
+
+    public enum CpuSpikeDuration
+    {
+        Short_5Seconds = 5,
+        Medium_10Seconds = 10,
+        Long_15Seconds = 15,
+        XLong_30Seconds = 30,
+        XXLong_60Seconds = 60
+    }
+
     [ApiController]
     [Route("api/[controller]")]
     public class SystemController : ControllerBase
@@ -15,16 +33,24 @@ namespace VCC.Assignment3.Controllers
         }
 
         [HttpPost("memory-spike")]
-        public IActionResult TriggerMemorySpike()
+        public IActionResult TriggerMemorySpike([FromQuery] MemorySpikeSize spikeSize = MemorySpikeSize.Small_500MB)
         {
             try
             {
                 var list = new List<byte[]>();
-                for (int i = 0; i < 100; i++)
+                int sizeMB = (int)spikeSize;
+                int chunks = sizeMB / 10;
+                
+                for (int i = 0; i < chunks; i++)
                 {
-                    list.Add(new byte[10 * 1024 * 1024]); // 10 MB
+                    list.Add(new byte[10 * 1024 * 1024]); // 10 MB chunks
                 }
-                return Ok(new { message = "Memory spike triggered" });
+                
+                return Ok(new { 
+                    message = "Memory spike triggered",
+                    sizeAllocatedMB = sizeMB,
+                    chunks = chunks
+                });
             }
             catch (Exception ex)
             {
@@ -33,16 +59,44 @@ namespace VCC.Assignment3.Controllers
         }
 
         [HttpPost("cpu-spike")]
-        public IActionResult TriggerCpuSpike()
+        public IActionResult TriggerCpuSpike([FromQuery] CpuSpikeDuration duration = CpuSpikeDuration.Short_5Seconds)
         {
             try
             {
+                int durationSeconds = (int)duration;
+                int targetMs = durationSeconds * 1000;
+                int processorCount = Environment.ProcessorCount;
+                int threadsToUse = (int)Math.Ceiling(processorCount * 0.8); // 80% of CPU cores
+                
                 var sw = Stopwatch.StartNew();
-                while (sw.ElapsedMilliseconds < 5000) // 5 seconds
+                var tasks = new List<Task>();
+                
+                for (int i = 0; i < threadsToUse; i++)
                 {
-                    Math.Sqrt(new Random().Next());
+                    tasks.Add(Task.Run(() =>
+                    {
+                        var localSw = Stopwatch.StartNew();
+                        while (localSw.ElapsedMilliseconds < targetMs)
+                        {
+                            // Perform CPU-intensive operations
+                            for (int j = 0; j < 1000; j++)
+                            {
+                                Math.Sqrt(Math.Pow(j, 2));
+                            }
+                        }
+                    }));
                 }
-                return Ok(new { message = "CPU spike triggered" });
+                
+                Task.WaitAll(tasks.ToArray());
+                
+                return Ok(new { 
+                    message = "CPU spike triggered",
+                    durationSeconds = durationSeconds,
+                    actualDurationMs = sw.ElapsedMilliseconds,
+                    cpuUtilizationTarget = "80%",
+                    threadsUsed = threadsToUse,
+                    totalProcessors = processorCount
+                });
             }
             catch (Exception ex)
             {
@@ -54,16 +108,20 @@ namespace VCC.Assignment3.Controllers
         public IActionResult GetSystemResources()
         {
             var process = Process.GetCurrentProcess();
-            var totalMemory = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes;
-            var usedMemory = process.WorkingSet64;
+            var totalMemoryBytes = GC.GetGCMemoryInfo().TotalAvailableMemoryBytes;
+            var usedMemoryBytes = process.WorkingSet64;
             var cpuTime = process.TotalProcessorTime.TotalMilliseconds;
             var systemName = RuntimeInformation.OSDescription;
+            
+            double totalMemoryGB = Math.Round(totalMemoryBytes / (1024.0 * 1024.0 * 1024.0), 2);
+            double usedMemoryGB = Math.Round(usedMemoryBytes / (1024.0 * 1024.0 * 1024.0), 2);
+            
             return Ok(new
             {
                 systemName,
-                totalMemory,
-                usedMemory,
-                cpuTime
+                totalMemoryGB,
+                usedMemoryGB,
+                cpuTimeMs = cpuTime
             });
         }
     }
