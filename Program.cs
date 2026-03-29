1b6165fe3589035b242ad04f5eb46f21300e2e41
Ashutosh Nigam <ashutosh.nigam@live.com>
2026-03-29 11:40:42 +0530

    enabled swagger

--- Program.cs
+++ Program.cs
@@ -16,11 +16,9 @@ builder.Services.AddSwaggerGen(c =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
